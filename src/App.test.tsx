51381d2ab0a06b7ee4fc3ec2389d07ffd1d493c5
Cloudbop <boppincolin@gmail.com>
2022-02-02 22:52:28 +0000

    fail render header+button

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders App header", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Game Of Life/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("renders a button", () => {
+  render(<App />);
+
+  const simpleButton = screen.getByRole("button", {
+    name: "Click Me",
+  });
+
+  expect(simpleButton).toBeEnabled();
 });
