a7e123e259dfafb2131e93566bd9c35fff9ec85c
Cloudbop <boppincolin@gmail.com>
2022-02-03 01:30:16 +0000

    pass test render grid

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import SimpleButton from "./UserInterface/SimpleButton";
+import { generateRandomTiles } from "./utils/utils";
 
 function App() {
+  const [grid, setGrid] = React.useState(() => {
+    return generateRandomTiles();
+  });
   return (
     <div className="App">
       <header className="App-header">
@@ -24,6 +28,24 @@ function App() {
       <div className="App-wrapper">
         <h2>Write a failing test...</h2>
         <SimpleButton />
+
+        <div>
+          {grid.map((rows, i) =>
+            rows.map((col, k) => (
+              <div
+                role="gridcell"
+                key={`${i}-${k}`}
+                style={{
+                  width: 20,
+                  height: 20,
+                  backgroundColor: grid[i][k] ? "#F68E5F" : undefined,
+                  border: "1px solid #595959",
+                }}
+                onClick={() => alert(`${i}:${k}hello ${grid[i][k]}`)}
+              />
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
