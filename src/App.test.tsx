6a03df6893c1032784d3654a167bb7e96a1c4fc8
Cloudbop <boppincolin@gmail.com>
2022-02-03 01:30:01 +0000

    failtest render2x2 grid

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,16 +4,21 @@ import App from "./App";
 
 test("renders App header", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Game Of Life/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Game Of Life/i);
+  expect(headerElement).toBeInTheDocument();
 });
-
 test("renders a button", () => {
   render(<App />);
-
   const simpleButton = screen.getByRole("button", {
     name: "Click Me",
   });
 
   expect(simpleButton).toBeEnabled();
+});
+test("renders a 2x2 grid", () => {
+  render(<App />);
+  //
+  const simpleGrid = screen.getAllByRole("gridcell");
+  // 2x2 grid
+  expect(simpleGrid.length === 4).toBe(true);
 });
