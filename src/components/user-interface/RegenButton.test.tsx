ae06b6d66b335ce9286a53ae09b44ee277dec027
Cloudbop <boppincolin@gmail.com>
2022-02-05 21:55:29 +0000

    +RegenButton f-test

---
+++ src/components/user-interface/RegenButton.test.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import RegenButton from "./RegenButton";
+
+test("renders Button", () => {
+  const handleChange = jest.fn();
+  //
+  render(<RegenButton handleClick={handleChange} />);
+  const tickButton = screen.getByRole("button", {
+    name: "Regenerate",
+  });
+
+  expect(tickButton).toBeEnabled();
+  userEvent.click(tickButton);
+  expect(handleChange).toHaveBeenCalledTimes(1);
+  // expect(headerElement).toBeInTheDocument();
+});
