77faebcf6da50f4c4e3365aac3b9d2770bbb00f0
Cloudbop <boppincolin@gmail.com>
2022-02-05 01:00:25 +0000

    pass - when a cell is clicked, invert the cell css state

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import SimpleButton from "./UserInterface/SimpleButton";
-import { generateRandomTiles } from "./utils/utils";
-
+import { generateRandomTiles, invertClickedCell } from "./utils/utils";
+const numCols = 2;
+const numRows = 2;
 function App() {
   const [grid, setGrid] = React.useState(() => {
-    return generateRandomTiles();
+    return generateRandomTiles(numCols, numRows);
   });
   return (
     <div className="App">
@@ -26,23 +27,33 @@ function App() {
         <h1> Game Of Life </h1>
       </header>
       <div className="App-wrapper">
-        <h2>Write a failing test...</h2>
         <SimpleButton />
 
-        <div>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: `repeat(${numCols}, 20px)`,
+            width: "fit-content",
+            margin: "0 auto",
+          }}
+        >
           {grid.map((rows, i) =>
             rows.map((col, k) => (
               <div
+                onClick={() => {
+                  const newGrid = invertClickedCell(grid, i, k);
+                  setGrid(newGrid);
+                }}
+                className={`cell ${grid[i][k] ? "on" : "off"}`}
                 role="gridcell"
                 key={`${i}-${k}`}
                 style={{
                   width: 20,
                   height: 20,
-                  backgroundColor: grid[i][k] ? "#F68E5F" : undefined,
-                  border: "1px solid #595959",
                 }}
-                onClick={() => alert(`${i}:${k}hello ${grid[i][k]}`)}
-              />
+              >
+                {grid[i][k]}
+              </div>
             ))
           )}
         </div>
