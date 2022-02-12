2c01fe6aa2d73338c1522981e70af6c1b7fbc74a
Cloudbop <boppincolin@gmail.com>
2022-02-12 23:28:06 +0000

    +lights out

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import GOL2dArray from "./components/gol-2d-array/GolTwoDimArray";
+import LightsOut from "./components/lights-out/LightsOut";
 
 function App() {
   return (
     <div className="App">
       <h1 className={"App-header"}>Experiments with Grid Games!</h1>
       <GOL2dArray />
+      <LightsOut />
     </div>
   );
 }
