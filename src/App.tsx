939027cd3796f526854e30444da74af85915dd2d
Cloudbop <boppincolin@gmail.com>
2022-02-12 19:03:05 +0000

    +updated styles, +cmntdout import

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import GOL2dArray from "./components/gol-2d-array/GolTwoDimArray";
 function App() {
   return (
     <div className="App">
-      <h1>Experiments with Grid Games!</h1>
+      <h1 className={"App-header"}>Experiments with Grid Games!</h1>
       <GOL2dArray />
     </div>
   );
